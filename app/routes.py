4a6c5dd0e3c641cd0dd8b06a2b5a8fe2a2cbc1be
Ricardo <rcordero231@gmail.com>
2024-01-09 10:32:56 -0800

    created new user

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,115 @@
+from flask import request
 from app import app
 from fake_data.posts import post_data
 
+
+users = []
+
+
 @app.route('/')
 def index():
-    first = 'Ricardo'
-    last = 'Cordero'
-    age = '24'
-    return f'Hello World -{first} {last} {age}'
+    first_name = 'Ricardo'
+    last_name = 'Cordero'
+    return f'Hello World!! - From {first_name} {last_name}'
+
+# USER ENDPOINTS
+
+# Creat new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Check to see if the request body is JSON
+    if not request.is_json:
+        return {'Error': 'Yout content-type must be application/json'}, 400
+    
+    # Get the data from the request body
+    data= request.json
+    
+    # Validate that the data has all of the required fields
+    required_fields = ['firstName', 'lastName', 'username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+        if missing_fields:
+            return {'error': f"{', '.join(missing_fields)} must be in the body"}, 400
+        
+    
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    
+    # Check to see if user is already in database (will be different when we use db)
+    for user in users:
+        if user['username'] == username or user['email'] == email:
+            return {'error': 'A user with that username and/or email already exists'}, 400
+    
+    # Create new user dict and append to users list
+    new_user = {
+        "id": len(users)+1,
+        "firstName": first_name,
+        "lastName": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+    users.append(new_user)
+    return 'This is the Create User endpoint'
 
 # POST ENDPOINTS
 
 # Get all posts
 @app.route('/posts')
 def get_posts():
+    # Get the posts from storage (fake data, will setup db tomorrow)
     posts = post_data
     return posts
 
-# get single post by id
+# Get single post by ID
 @app.route('/posts/<int:post_id>')
-def get_post_id(post_id):
-    # print(f'The post ID is {post_id} and the type is {type(post_id)}')
-    # return str(post_id)
+def get_post(post_id):
+    # Get the posts from storage
     posts = post_data
-    #for each dictionary in the list of post dictionaries
+    # For each dictionary in the list of post dictionaries
     for post in posts:
-        # If the key of 'id' on the post dictionary matches the post_id from URL
+        # if the key of 'id' on the post dictionary matches the post_id from the URL
         if post['id'] == post_id:
-            #return the post of the matching 'id'
+            # Return that post dictionary
             return post
-    return {'ERROR': f'Post with an ID of {post_id} does NOT exist'}
-        
+    # If we loop through all of the posts without returning, the post with that ID does not exist
+    return {'error': f'Post with an ID of {post_id} does not exist'}, 404
+
+# Create new Post route
+@app.route('/posts', methods=['POST'])
+def create_post():
+    # Check to see that the request body is JSON
+    if not request.is_json:
+        return {'error': 'You content-type must be application/json'}, 400
+    # Get the data from the request body
+    data = request.json
+    # Validate the incoming data
+    required_fields = ['title', 'body']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    # Get data from the body
+    title = data.get('title')
+    body = data.get('body')
+
+    # Create a new post with data
+    new_post = {
+        "id": len(post_data) + 1,
+        "title": title,
+        "body": body,
+        "userId": 1,
+        "dateCreated": "2024-01-09T11:25:45",
+        "likes": 0
+    }
+    # Add the new post to the list of posts
+    post_data.append(new_post)
+    return new_post, 201

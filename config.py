b084a18dbfe715d0b0e472d547dafc44d061ccf6
Ricardo <rcordero231@gmail.com>
2024-01-12 09:59:28 -0800

    git commit

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+
 
 # get the base directory of this folder
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,4 +8,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # 'C:\\Users\\rcord\\Documents\\codingtemple-kekambas-137\\week6\\FLASK_API'
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
